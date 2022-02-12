ea94a85818dd019921c7931f0a87f2fc227b48a6
fvn70 <fvn_reg@mail.ru>
2022-02-12 20:02:22 +0700

    new Stage 2/6

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,12 +1,22 @@
-def compare(reg, inp):
+def comp_beg(reg, inp):
     if not reg:
         return True
     if not inp:
         return False
-    if reg == '.' or reg == inp:
-        return True
-    else:
+    if reg[0] not in '.' and reg[0] != inp[0]:
         return False
+    return comp_beg(reg[1:], inp[1:])
+
+
+def compare(reg, inp):
+    if not reg:
+        return True
+    while inp:
+        if comp_beg(reg, inp):
+            return True
+        else:
+            inp = inp[1:]
+    return False
 
 
 reg, inp = input().split('|')

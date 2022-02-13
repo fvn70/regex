bacd4c81e6cda053afcab8bad2b1fd7a12aa79b4
fvn70 <fvn_reg@mail.ru>
2022-02-13 12:24:50 +0700

    new Stage 6/6

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,24 +1,27 @@
 def comp_beg(reg, inp):
     if not reg:
         return True
-    elif not inp:
+    if not inp:
         return False
-    elif '?' in reg and reg[1] == '?':
-        if reg[0] == inp[0] and reg[2] == inp[1]:
-            return True
-        return comp_beg(reg[2:], inp)
-    elif ('*' in reg or '+' in reg) and reg[1] in '*+':
-        if reg[0] == inp[0]:
-            return True
-        if reg[0] == '.':
-            if len(reg) < 3 or reg[2] == inp[0]:
+    if reg[0] == '\\':
+        return reg[1] == inp[0]
+    if len(reg) > 1 and reg[1] in '?+*':
+        if '?' in reg and reg[1] == '?':
+            if reg[0] == inp[0] and reg[2] == inp[1]:
                 return True
-            return comp_beg(reg, inp[1:])
-        return comp_beg(reg[2:], inp) if '*' in reg else False
-    elif reg[0] not in '.' and reg[0] != inp[0]:
+            return comp_beg(reg[2:], inp)
+        elif ('*' in reg or '+' in reg) and reg[1] in '*+':
+            if reg[0] == inp[0]:
+                return True
+            if reg[0] == '.':
+                if len(reg) < 3 or reg[2] == inp[0]:
+                    return True
+                return comp_beg(reg, inp[1:])
+            return comp_beg(reg[2:], inp) if '*' in reg else False
+    if reg[0] not in '.\\' and reg[0] != inp[0]:
         return False
-    else:
-        return comp_beg(reg[1:], inp[1:])
+    return comp_beg(reg[1:], inp[1:])
+
 
 def compare(reg, inp):
     if not reg:
@@ -32,6 +35,8 @@ def compare(reg, inp):
     if f_beg:
         is_beg = comp_beg(reg0, inp)
     if f_end:
+        if '\\' in reg0 and reg0[-1] in '.?*+' and reg0[-2] == '\\':
+            return reg0[-1] == inp[-1]
         inp0 = inp if f_mult else inp[-len(reg0):]
         if f_last:
             is_end = reg0[-1] == '.' or reg0[-2] in ['.', inp0[-1]]

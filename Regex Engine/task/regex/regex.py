30f169e2821c422c63b2385e1d30705ebab3efe6
fvn70 <fvn_reg@mail.ru>
2022-02-13 09:54:15 +0700

    new Stage 4/6

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -11,6 +11,25 @@ def comp_beg(reg, inp):
 def compare(reg, inp):
     if not reg:
         return True
+    reg0 = reg.lstrip('^').rstrip('$')
+    f_beg = reg and reg[0] == '^'
+    f_end = reg and reg[-1] == '$'
+    f_mult = '*' in reg or '+' in reg
+    f_last = reg0[-1] in '.*+'
+    is_beg, is_end = True, True
+
+    if f_beg:
+        is_beg = comp_beg(reg0, inp)
+    if f_end:
+        inp0 = inp[-len(reg0):]
+        if f_last:
+            is_end = reg0[-1] == '.' or reg0[-2] in ['.', inp0[-1]]
+        else:
+            is_end = reg0[-1] == inp0[-1]
+        is_end = is_end and comp_beg(reg0, inp0)
+    if f_beg or f_end:
+        return is_beg and is_end
+
     while inp:
         if comp_beg(reg, inp):
             return True
